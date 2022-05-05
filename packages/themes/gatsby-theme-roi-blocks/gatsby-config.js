6c05cd5d6fdab35368045f9bf67a3ed9c032ce32
d-hancock <hancockdale8@gmail.com>
2022-05-05 15:29:34 -0500

    added beginings of a theme for the block library implementation

---
+++ packages/themes/gatsby-theme-roi-blocks/gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteMetadata: {
+    title: `gatsby-theme-roi-blocks`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+    plugins: [
+      'gatsby-plugin-top-layout',
+      // If you want to use styled components you should add the plugin here.
+      // 'gatsby-plugin-styled-components',
+      'gatsby-plugin-mui-emotion',
+      'gatsby-plugin-react-helmet',
+      'gatsby-plugin-resolve-src',
+    ],
+}
