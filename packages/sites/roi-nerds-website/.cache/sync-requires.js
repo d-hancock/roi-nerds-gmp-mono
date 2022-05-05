936095cddfb2f96cbc254e2a38f1028d884913bc
d-hancock <hancockdale8@gmail.com>
2022-05-05 16:27:54 -0500

    Setup idiot index and about pages.

--- packages/sites/roi-nerds-website/.cache/sync-requires.js
+++ packages/sites/roi-nerds-website/.cache/sync-requires.js
@@ -6,6 +6,7 @@ const preferDefault = m => (m && m.default) || m
 exports.components = {
   "component---cache-dev-404-page-js": preferDefault(require("/home/dale/Development/roi-nerds-gmp-mono/packages/sites/roi-nerds-website/.cache/dev-404-page.js")),
   "component---src-pages-404-js": preferDefault(require("/home/dale/Development/roi-nerds-gmp-mono/packages/sites/roi-nerds-website/src/pages/404.js")),
+  "component---src-pages-about-js": preferDefault(require("/home/dale/Development/roi-nerds-gmp-mono/packages/sites/roi-nerds-website/src/pages/about.js")),
   "component---src-pages-index-js": preferDefault(require("/home/dale/Development/roi-nerds-gmp-mono/packages/sites/roi-nerds-website/src/pages/index.js"))
 }
 
