57569e86083d574fee69856455abd259c03fc7f0
d-hancock <hancockdale8@gmail.com>
2022-05-06 17:07:38 -0500

    implementing gatsby-node

---
+++ packages/themes/gatsby-theme-roi-core/gatsby-node.js
@@ -0,0 +1,9 @@
+// import {
+//   onCreateNode as _onCreateNode,
+//   createSchemaCustomization as _createSchemaCustomization,
+//   createPages as _createPages,
+// } from "./gatsby/gatsby-node"
+
+// export const onCreateNode = _onCreateNode
+// export const createSchemaCustomization = _createSchemaCustomization
+// export const createPages = _createPages
