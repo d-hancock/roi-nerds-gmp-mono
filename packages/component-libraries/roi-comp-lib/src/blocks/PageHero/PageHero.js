9b503dea2ac50b6e5971214cc31a711bc8283cfb
d-hancock <hancockdale8@gmail.com>
2022-05-30 17:03:29 -0500

    removed nav const from PageHero

--- packages/component-libraries/roi-comp-lib/src/blocks/PageHero/PageHero.js
+++ packages/component-libraries/roi-comp-lib/src/blocks/PageHero/PageHero.js
@@ -4,13 +4,6 @@ import { Fragment } from "react"
 import { Popover, Transition } from "@headlessui/react"
 import { MenuIcon, XIcon } from "@heroicons/react/outline"
 
-const navigation = [
-  { name: "Product", href: "#" },
-  { name: "Features", href: "#" },
-  { name: "Marketplace", href: "#" },
-  { name: "Company", href: "#" },
-]
-
 export const PageHero = ({
   beginingTitle,
   highlightTitle,
