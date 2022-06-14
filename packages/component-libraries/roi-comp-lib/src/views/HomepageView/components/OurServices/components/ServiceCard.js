7503ec704b044e8f3a23537ebac14eb050ab5a23
d-hancock <hancockdale8@gmail.com>
2022-06-13 20:35:54 -0500

    Working on homepage sections

---
+++ packages/component-libraries/roi-comp-lib/src/views/HomepageView/components/OurServices/components/ServiceCard.js
@@ -0,0 +1,22 @@
+import React from "react"
+import tw, { css } from "twin.macro"
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardHeader,
+  Typography,
+} from "@mui/material"
+
+const ServiceCard = () => {
+  return (
+    <Card>
+      <CardContent>
+        <h3 tw="text-green">I am the Header</h3>
+        <Typography>I'm a quick blerb about the type of service</Typography>
+      </CardContent>
+    </Card>
+  )
+}
+
+export default ServiceCard
